3a0e6522f3ec1428e319d09016f41d041e821c1e
Tim Peters <mail@darksecond.nl>
2023-10-23 22:11:23 +0200

    Make program at least compile and work

--- AllyRemapper/Program.cs
+++ AllyRemapper/Program.cs
@@ -1,15 +1,11 @@
 ﻿using Ally.HID;
 
-Console.WriteLine("Hello, World!");
-
-AllyUSB.SendDefaultLayout(DefaultLayout.Desktop);
-AllyUSB.SendNext();
-AllyUSB.SendMapping(ButtonCode.LT_RT, new Mapping[] { Mapping.LEFT_CLICK, Mapping.DISABLED, Mapping.RIGHT_CLICK, Mapping.DISABLED, });
-AllyUSB.SendNext();
-AllyUSB.SendMapping(ButtonCode.LB_RB, new Mapping[] { Mapping.DISABLED, Mapping.DISABLED, Mapping.DISABLED, Mapping.DISABLED, });
+Controller.GetDefaultDesktopController().Apply();
+Console.WriteLine("Applied Desktop Layout");
 
 Console.ReadKey();
 
-AllyUSB.SendDefaultLayout(DefaultLayout.Xbox);
+Controller.GetDefaultXboxController().Apply();
+Console.WriteLine("Applied Xbox Layout");
 
 Console.ReadKey();
